ca6b89b1bb1c1baa25fbae8886c5ee5f95cfd83e
miguelrey0415 <101655447+miguelrey0415@users.noreply.github.com>
2022-03-31 07:21:40 -0600

    Add files via upload

---
+++ GoogleCharts/js/g5.js
@@ -0,0 +1,47 @@
+//GRAFICO5
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Sales', 'Expenses'],
+    ['2013',  1000,      400],
+    ['2014',  1170,      460],
+    ['2015',  660,       1120],
+    ['2016',  1030,      540]
+  ]);
+
+  var options = {
+    title: 'Company Performance',
+    hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+    vAxis: {minValue: 0}
+  };
+
+  var chart = new google.visualization.AreaChart(document.getElementById('graf5'));
+  chart.draw(data, options);
+}
+
+//GRAFICO6
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Director (Year)',  'Rotten Tomatoes', 'IMDB'],
+    ['Alfred Hitchcock (1935)', 8.4,         7.9],
+    ['Ralph Thomas (1959)',     6.9,         6.5],
+    ['Don Sharp (1978)',        6.5,         6.4],
+    ['James Hawes (2008)',      4.4,         6.2]
+  ]);
+
+  var options = {
+    title: 'The decline of \'The 39 Steps\'',
+    vAxis: {title: 'Accumulated Rating'},
+    isStacked: true
+  };
+
+  var chart = new google.visualization.SteppedAreaChart(document.getElementById('graf6'));
+
+  chart.draw(data, options);
+}
